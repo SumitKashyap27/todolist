cd08aa96078ebc697b08e20e988a9264955b44f7
Sumit Kashyap <sumit20020927@gmail.com>
2024-09-23 17:39:19 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -1,14 +1,20 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 const page = () => {
+  const [title, settitle] = useState("")
+  const [desc, setdesc] = useState("")
   return (
     <>
     <h1 className='bg-black text-white p-5 text-xl text-center font-bold'   >
       Todo List
     </h1>
     <form className='text-center'>
-      <input type="text" className='text-2xl border-zinc-800 border-2 m-5 px-4 py-2 text-center'
-      placeholder='Enter your first Task' />
+      <input type="text" className='rounded-md text-2xl border-zinc-800 border-2 m-5 px-4 py-2 text-center'
+      placeholder='Enter your Task' />
+      <input type="text" className='rounded-md text-2xl border-zinc-800 border-2 m-5 px-4 py-2 text-center'
+      placeholder='Enter description' />
+      <button className='bg-black px-6 py-2 text-white rounded-md '>Add Text </button>
     </form>
     </>
   )
