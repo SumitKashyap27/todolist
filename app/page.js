c815a9247009eee1b3687b70bcb4527ba032edf0
Sumit Kashyap <sumit20020927@gmail.com>
2024-09-26 00:35:32 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -1,19 +1,25 @@
 "use client"
 import React, { useState } from 'react'
+import { MdDeleteForever } from "react-icons/md";
 
-const page = () => {
-  const [title, settitle] = useState("")
-  const [desc, setdesc] = useState("")
-  const [mainTask, setmainTask] = useState([]) // empty array to store tasks
+const Page = () => {
+  const [title, setTitle] = useState("")
+  const [desc, setDesc] = useState("")
+  const [mainTask, setMainTask] = useState([]) // empty array to store tasks
 
   const submitHandler = (e) => {
     e.preventDefault() // Prevent page reload
     // Add task as an object with title and desc
-    setmainTask([...mainTask, { title, desc }]) 
+    setMainTask([...mainTask, { title, desc }])
     // Clear form fields
-    settitle("")
-    setdesc("")
-    console.log(mainTask)
+    setTitle("")
+    setDesc("")
+  }
+
+  const deleteHandler = (i) => {
+    let copyTask = [...mainTask]
+    copyTask.splice(i, 1)
+    setMainTask(copyTask)
   }
 
   let renderTask = <h2>No task available</h2>
@@ -21,11 +27,15 @@ const page = () => {
   if (mainTask.length > 0) {
     renderTask = mainTask.map((t, i) => {
       return (
-        <li key={i}>
-          <div className='flex justify-between'>
-            <h5>{t.title}</h5>
-            <h6>{t.desc}</h6>
+        <li key={i} className='flex mb-'>
+          <div className='flex justify-evenly m-5 w-2/3'>
+            <h5 className='text-2xl font-semibold'>{t.title}</h5>
+            <h6 className='text-xl font-semibold'>{t.desc}</h6>
           </div>
+          <button className='text-4xl p-4 h-12 w-12 text-red-400' 
+            onClick={() => deleteHandler(i)}> 
+            <MdDeleteForever /> 
+          </button>
         </li>
       );
     });
@@ -40,15 +50,15 @@ const page = () => {
         <input type="text" className='rounded-md text-2xl border-zinc-800 border-2 m-5 px-4 py-2 text-center'
           placeholder='Enter your title'
           value={title}
-          onChange={(e) => settitle(e.target.value)} />
+          onChange={(e) => setTitle(e.target.value)} />
         <input type="text" className='rounded-md text-2xl border-zinc-800 border-2 m-5 px-4 py-2 text-center'
           placeholder='Enter description'
           value={desc}
-          onChange={(e) => setdesc(e.target.value)} />
+          onChange={(e) => setDesc(e.target.value)} />
         <button className='bg-black px-6 py-2 text-white rounded-md'>Add Task</button>
       </form>
       <hr />
-      <div className='p-8 bg-slate-200'>
+      <div className='p-8 bg-slate-100'>
         <ul>
           {renderTask}
         </ul>
@@ -57,4 +67,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Page
